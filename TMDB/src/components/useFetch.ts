6e3db4fff150ed2d670f77f94018ac25a554e3b8
김예안 <kjan0929@naver.com>
2025-04-11 09:43:43 +0900

    refactor: 커스텀 훅으로 데이터 패칭

---
+++ TMDB/src/components/useFetch.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface ApiResponse<T> {
+  data: T | null;
+  isPending: boolean;
+  isError: boolean;
+}
+
+const useFetch = <T>(url: string): ApiResponse<T> => {
+  const [data, setData] = useState<T | null>(null);
+  const [isPending, setIsPending] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsPending(true);
+
+      try {
+        const { data } = await axios.get<T>(url, {
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
+          },
+        });
+        console.log(data);
+        setData(data);
+      } catch {
+        setIsError(true);
+      } finally {
+        setIsPending(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, isPending, isError };
+};
+
+export default useFetch;
--- TMDB/src/pages/MoviePage.tsx
+++ TMDB/src/pages/MoviePage.tsx
@@ -1,40 +1,18 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Movie, MovieResponse } from "../types/movies";
+import { useState } from "react";
+import { MovieResponse } from "../types/movies";
 import MovieCard from "../components/MovieCard";
 import LoadingSpinner from "../components/LoadingSpinner";
 import { useParams } from "react-router-dom";
+import useFetch from "../components/useFetch";
 
 export default function MoviePage() {
   const { category } = useParams<{ category: string }>();
 
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [isPending, setIsPending] = useState(false);
-  const [isError, setIsError] = useState(false);
   const [page, setPage] = useState(1);
 
-  useEffect(() => {
-    const fetchMovies = async () => {
-      setIsPending(true);
-      try {
-        const { data } = await axios<MovieResponse>(
-          `https://api.themoviedb.org/3/movie/${category}?language=ko-KR&page=${page}`,
-          {
-            headers: {
-              Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
-            },
-          }
-        );
-        setMovies(data.results);
-      } catch {
-        setIsError(true);
-      } finally {
-        setIsPending(false);
-      }
-    };
+  const url = `https://api.themoviedb.org/3/movie/${category}?language=ko-KR&page=${page}`;
 
-    fetchMovies();
-  }, [category, page]);
+  const { data: movies, isPending, isError } = useFetch<MovieResponse>(url);
 
   if (isError) {
     return (
@@ -73,8 +51,9 @@ export default function MoviePage() {
 
       {!isPending && (
         <div className="py-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-          {movies &&
-            movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+          {movies?.results.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
         </div>
       )}
     </>
